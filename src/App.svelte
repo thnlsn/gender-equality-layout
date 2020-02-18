f1e9d2f6b5ec515041a98185d3588f73383d61c8
Thomas Nelson <thnlsn@gmail.com>
2020-02-17 23:31:18 -0800

    app component divided in half

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script>
-	export let name;
+  /*   export let name; */
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  /*   h1 {
+    color: purple;
+  } */
 </style>
 
-<h1>Hello {name}!</h1>
+<div class="row no-gutters">
+  <div class="col">
+    <div class="left-side" />
+  </div>
+  <div class="col">
+    <div class="right-side" />
+  </div>
+</div>
